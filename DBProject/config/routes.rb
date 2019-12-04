7d997a91666cc390b1092b25f71e590dee753a6e
eduardo-soto1997 <50255822+eduardo-soto1997@users.noreply.github.com>
2019-12-04 10:50:10 -0600

    Here is my new stuff lol 12-4-2019

--- DBProject/config/routes.rb
+++ DBProject/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  root 'welcome#index'
 
   resources :invoices
   resources :addresses
@@ -15,6 +14,7 @@ Rails.application.routes.draw do
   resources :vendor_pos
   resources :vendors
   get 'db_project/index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
